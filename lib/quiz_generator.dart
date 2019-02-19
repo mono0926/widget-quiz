8ca34a720f49bc2a48e1d2a94f5a2b6ff4cdccd7
Masayuki Ono <mono0926@gmail.com>
2019-02-19 10:55:46 +0900

    Fix quiz data

--- lib/quiz_generator.dart
+++ lib/quiz_generator.dart
@@ -6,14 +6,16 @@ import 'quiz.dart';
 import 'w.dart';
 
 Future<List<Quiz>> load() async {
-  final ws = ((jsonDecode(await rootBundle.loadString('assets/widgets.json'))
-          as List)
-      .map((j) => W.fromJson(j as Map<String, dynamic>))
-      .toList())
-    ..shuffle();
-  return ws.sublist(0, 10).map((c) {
+  final ws =
+      ((jsonDecode(await rootBundle.loadString('assets/widgets.json')) as List)
+          .map((j) => W.fromJson(j as Map<String, dynamic>))
+          .toList());
+  return (ws..shuffle()).sublist(0, 10).map((c) {
     return Quiz(
         correct: c,
-        others: ws.where((w) => w.name != c.name).toList().sublist(0, 3));
+        others: (ws..shuffle())
+            .where((w) => w.name != c.name)
+            .toList()
+            .sublist(0, 3));
   }).toList();
 }
